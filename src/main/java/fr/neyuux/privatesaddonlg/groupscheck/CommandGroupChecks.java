05c3e4665eb285783b11fbd977848d375a4e9c9d
Neyuux <60991177+Neyuux@users.noreply.github.com>
2023-06-03 03:37:14 +0200

    push CommandGroupChecks

---
+++ src/main/java/fr/neyuux/privatesaddonlg/groupscheck/CommandGroupChecks.java
@@ -0,0 +1,23 @@
+package fr.neyuux.privatesaddonlg.groupscheck;
+
+import fr.neyuux.privatesaddonlg.Plugin;
+import fr.ph1lou.werewolfapi.annotations.AdminCommand;
+import fr.ph1lou.werewolfapi.commands.ICommand;
+import fr.ph1lou.werewolfapi.enums.StateGame;
+import fr.ph1lou.werewolfapi.game.WereWolfAPI;
+import fr.ph1lou.werewolfapi.player.interfaces.IPlayerWW;
+import org.bukkit.entity.Player;
+
+@AdminCommand(key = "privatesaddon.commands.admin.groupchecks.command", descriptionKey = "privatesaddon.commands.admin.groupchecks.description", argNumbers = {1}, statesGame = {StateGame.GAME})
+public class CommandGroupChecks implements ICommand {
+    public void execute(WereWolfAPI game, Player player, String[] args) {
+        IPlayerWW playerWW1 = game.getPlayersWW().stream().filter(iPlayerWW -> iPlayerWW.getName().equalsIgnoreCase(args[0])).findFirst().orElse(null);
+
+        if (playerWW1 == null) {
+            player.sendMessage(Plugin.getPrefix() + "§cJoueur introuvable.");
+            return;
+        }
+
+        player.sendMessage(Plugin.getPrefix() + "§cNombres d'irrespects des groupes : §b§l" + Plugin.getINSTANCE().getGroupsWarning().get(playerWW1.getUUID()) + "§b§o(§l" + Plugin.getINSTANCE().getGroupsWarningRatio(playerWW1.getUUID()) + "§b§o / minute)");
+    }
+}
