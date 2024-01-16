e0a6ca408ff502c2842d56ce780b53be161a93f4
Neyuux <60991177+Neyuux@users.noreply.github.com>
2024-01-16 22:32:07 +0100

    correct Illusionist key

--- src/main/java/fr/neyuux/privatesaddonlg/roles/Illusionist.java
+++ src/main/java/fr/neyuux/privatesaddonlg/roles/Illusionist.java
@@ -25,7 +25,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @Role(
-        key = "privates.roles.illusionist.display",
+        key = "privatesaddon.roles.illusionist.display",
         category = Category.VILLAGER,
         attribute = RoleAttribute.VILLAGER
 )
